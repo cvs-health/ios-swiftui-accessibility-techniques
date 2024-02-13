a188928fda24248097f6767771dcefb14132b673
Paul J. Adam <paul@pauljadam.com>
2024-02-12 18:38:42 -0600

    Update PopoversView.swift

--- iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/PopoversView.swift
+++ iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/PopoversView.swift
@@ -51,7 +51,7 @@ struct PopoversView: View {
                     Text("License Agreement")
                         .font(.headline)
                         .padding()
-                        .accessibilityAddTraits(/*@START_MENU_TOKEN@*/.isHeader/*@END_MENU_TOKEN@*/)
+                        .accessibilityAddTraits(.isHeader)
                     Text("""
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         """)

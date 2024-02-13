5970d463121b01c659f7bdfe18570ae373f92b94
Paul J. Adam <paul@pauljadam.com>
2024-02-12 18:43:46 -0600

    Update SheetsView.swift

--- iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/SheetsView.swift
+++ iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/SheetsView.swift
@@ -53,7 +53,7 @@ struct SheetsView: View {
                     VStack {
                         Text("License Agreement")
                             .font(.title)
-                            .accessibilityAddTraits(/*@START_MENU_TOKEN@*/.isHeader/*@END_MENU_TOKEN@*/)
+                            .accessibilityAddTraits(.isHeader)
                         Text("""
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             """)

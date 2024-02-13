f10a8cfdaa41c96ff75df2734ca02b3fed75052f
Paul J. Adam <paul@pauljadam.com>
2024-02-12 22:18:46 -0600

    Update PickersView.swift

--- iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/PickersView.swift
+++ iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/PickersView.swift
@@ -28,19 +28,14 @@ enum Fruit: String, CaseIterable, Identifiable {
  
 struct PickersView: View {
     @State private var selectedFruit: Fruit = .apple
-    
-    @State private var date = Date()
-    @State private var dateStart = Date()
-    @State private var dateEnd = Date()
-    @State private var time = Date()
-    let dateRange: ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let startComponents = DateComponents(year: 2021, month: 1, day: 1)
-        let endComponents = DateComponents(year: 2021, month: 12, day: 31, hour: 23, minute: 59, second: 59)
-        return calendar.date(from:startComponents)!
-            ...
-            calendar.date(from:endComponents)!
-    }()
+    @State private var selectedFruitDefault: Fruit = .banana
+    @State private var selectedFruitWheel: Fruit = .cherry
+    @State private var selectedFruitSegmented: Fruit = .grape
+    @State private var selectedFruitBad: Fruit = .apple
+    @State private var selectedFruitDefaultBad: Fruit = .banana
+    @State private var selectedFruitWheelBad: Fruit = .cherry
+    @State private var selectedFruitSegmentedBad: Fruit = .grape
+    @AccessibilityFocusState private var isTriggerFocused: Bool
     
     private var darkGreen = Color(red: 0 / 255, green: 102 / 255, blue: 0 / 255)
     private var darkRed = Color(red: 220 / 255, green: 20 / 255, blue: 60 / 255)
@@ -49,7 +44,7 @@ struct PickersView: View {
     var body: some View {
         ScrollView {
             VStack {
-                Text("Pickers are used to select dates and times. Pickers without the `.graphical` or `.wheel` style need an `.accessibilityLabel` set to match their visible label text. Pickers with the `.graphical` or `.wheel` style need visible `DatePicker(\"Label\")` text for each picker so it is spoken to VoiceOver as the accessibility label.")
+                Text("Pickers need visible label text and an accessibility label. Pickers with the default or `MenuPickerStyle` need `Picker(\"Label\")` text which is spoken to VoiceOver as the accessibility label. Pickers with the `WheelPickerStyle` or `SegmentedPickerStyle` need an `.accessibilityLabel` set to match their visible label text and need `.accessibilityElement(children: .contain)` or else the accessibility label will not be spoken to VoiceOver. Don't use `.accessibilityLabel` on `Picker` with the default or `MenuPickerStyle` or else VoiceOver will not speak the visible picker value text when the picker is closed. Use `AccessibilityFocusState` to send VoiceOver focus back to the picker when the value has been changed.")
                     .padding(.bottom)
                 Text("Good Examples")
                     .font(.subheadline)
@@ -66,39 +61,56 @@ struct PickersView: View {
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .accessibilityAddTraits(.isHeader)
-                Text("Fruit: \(selectedFruit.rawValue)")
+                Text("Fruit: \(selectedFruitDefault.rawValue)")
                     .frame(maxWidth: .infinity, alignment: .leading)
-                Picker("Fruit", selection: $selectedFruit) {
+                Picker("Fruit", selection: $selectedFruitDefault) {
                     ForEach(Fruit.allCases) { fruit in
                         Text(fruit.rawValue).tag(fruit)
                     }
                 }.frame(maxWidth: .infinity, alignment: .leading)
+                    .onChange(of: selectedFruitDefault) {
+                        isTriggerFocused = true
+                    }
+                    .accessibilityFocused($isTriggerFocused)
+                DisclosureGroup("Details") {
+                    Text("The default style good example uses `Picker(\"Fruit\")` label text which is spoken as the accessibility label to VoiceOver. `AccessibilityFocusState` is used to send VoiceOver focus back to the picker when the value has been changed.")
+                }.padding()
                 Text("Good Example Wheel Style")
                     .font(.subheadline)
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .accessibilityAddTraits(.isHeader)
-                Text("Fruit: \(selectedFruit.rawValue)")
+                Text("Fruit: \(selectedFruitWheel.rawValue)")
                     .frame(maxWidth: .infinity, alignment: .leading)
-                Picker("Fruit", selection: $selectedFruit) {
+                Picker("Fruit", selection: $selectedFruitWheel) { //a11yLabel and Picker label don't speak to VoiceOver for WheelPickerStyle
                     ForEach(Fruit.allCases) { fruit in
                         Text(fruit.rawValue).tag(fruit)
                     }
                 }
                 .pickerStyle(WheelPickerStyle())
+                .accessibilityElement(children: .contain) // only if children .contain applied then a11yLabel will speak
+                .accessibilityLabel("Fruit")
+                DisclosureGroup("Details") {
+                    Text("The wheel style good example uses `.accessibilityLabel(\"Fruit\")` which matches the visible label text and `.accessibilityElement(children: .contain)` to make sure the accessibility label is spoken to VoiceOver.")
+                }.padding()
                 Text("Good Example Segmented Style")
                     .font(.subheadline)
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .accessibilityAddTraits(.isHeader)
-                Text("Fruit: \(selectedFruit.rawValue)")
+                Text("Fruit: \(selectedFruitSegmented.rawValue)")
                     .frame(maxWidth: .infinity, alignment: .leading)
-                Picker("Fruit", selection: $selectedFruit) {
+                Picker("Fruit", selection: $selectedFruitSegmented) { //a11yLabel and Picker label don't speak to VoiceOver for SegmentedPickerStyle
                     ForEach(Fruit.allCases) { fruit in
                         Text(fruit.rawValue).tag(fruit)
                     }
                 }
                 .pickerStyle(SegmentedPickerStyle())
+                .accessibilityElement(children: .contain) // only if children .contain applied then a11yLabel will speak
+                .accessibilityLabel("Fruit")
+                DisclosureGroup("Details") {
+                    Text("The segmented style good example uses `.accessibilityLabel(\"Fruit\")` which matches the visible label text and `.accessibilityElement(children: .contain)` to make sure the accessibility label is spoken to VoiceOver.")
+                }.padding()
                 Text("Good Example Menu Style")
                     .font(.subheadline)
                     .fontWeight(.bold)
@@ -113,7 +125,7 @@ struct PickersView: View {
                 }.frame(maxWidth: .infinity, alignment: .leading)
                 .pickerStyle(MenuPickerStyle())
                 DisclosureGroup("Details") {
-                    Text("The first good Pickers example uses `.accessibilityLabel` on each `DatePicker` that matches the visible label text.")
+                    Text("The menu style good example uses `Picker(\"Fruit\")` label text which is spoken as the accessibility label to VoiceOver.")
                 }.padding()
                 Text("Bad Examples")
                     .font(.subheadline)
@@ -125,8 +137,61 @@ struct PickersView: View {
                     .frame(height: 2.0, alignment:.leading)
                     .background(colorScheme == .dark ? Color(.systemRed) : darkRed)
                     .padding(.bottom)
+                Text("Bad Example Default Style")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .accessibilityAddTraits(.isHeader)
+                Picker("", selection: $selectedFruitDefaultBad) {
+                    ForEach(Fruit.allCases) { fruit in
+                        Text(fruit.rawValue).tag(fruit)
+                    }
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                    .accessibilityLabel("Fruit")
                 DisclosureGroup("Details") {
-                    Text("The second bad Pickers example uses no visible `DatePicker(\"\")` text for each picker so nothing is spoken to VoiceOver as the accessibility label.")
+                    Text("The default style bad example uses `.accessibilityLabel(\"Fruit\")` which incorrectly overrides the visible picker label text and VoiceOver will not speak the visible selected picker value when the picker is closed. `AccessibilityFocusState` is not used to send VoiceOver focus back to the picker when the value has been changed.")
+                }.padding()
+                Text("Bad Example Wheel Style")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .accessibilityAddTraits(.isHeader)
+                Picker("", selection: $selectedFruitWheelBad) {
+                    ForEach(Fruit.allCases) { fruit in
+                        Text(fruit.rawValue).tag(fruit)
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                DisclosureGroup("Details") {
+                    Text("The wheel style bad example does not use an `.accessibilityLabel` or visible label text and does not use `.accessibilityElement(children: .contain)` to make sure the accessibility label is spoken to VoiceOver.")
+                }.padding()
+                Text("Bad Example Segmented Style")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .accessibilityAddTraits(.isHeader)
+                Picker("", selection: $selectedFruitSegmentedBad) {
+                    ForEach(Fruit.allCases) { fruit in
+                        Text(fruit.rawValue).tag(fruit)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                DisclosureGroup("Details") {
+                    Text("The segmented style bad example does not use an `.accessibilityLabel` or visible label text and does not use `.accessibilityElement(children: .contain)` to make sure the accessibility label is spoken to VoiceOver.")
+                }.padding()
+                Text("Bad Example Menu Style")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .accessibilityAddTraits(.isHeader)
+                Picker("", selection: $selectedFruitBad) {
+                    ForEach(Fruit.allCases) { fruit in
+                        Text(fruit.rawValue).tag(fruit)
+                    }
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                .pickerStyle(MenuPickerStyle())
+                DisclosureGroup("Details") {
+                    Text("The menu style bad example uses empty `Picker(\"\")` label text so there is no accessibility label spoken to VoiceOver.")
                 }.padding()
             }
             .navigationTitle("Pickers")

6df30e087fc3b8bf9b83e28322001d564d914c8d
Paul J. Adam <paul@pauljadam.com>
2026-01-21 14:38:31 -0600

    update copyright year

--- iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/ContentView.swift
+++ iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/ContentView.swift
@@ -1,5 +1,5 @@
 /*
-   Copyright 2023-2025 CVS Health and/or one of its affiliates
+   Copyright 2023-2026 CVS Health and/or one of its affiliates
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.

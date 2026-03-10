e4d374cab9118c64f1590eaf378835c10b8484b7
Adam, Paul <Paul.Adam2+cvsh@CVSHealth.com>
2026-03-10 12:36:16 -0500

    readme

--- A11yAgent/Package.swift
+++ A11yAgent/Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        // Pinned below 1.6 to avoid Swift 6.2 SendableMetatype path (fails under Homebrew's SDK; apple/swift-argument-parser#827).
+        .package(url: "https://github.com/apple/swift-argument-parser.git", "1.3.0"..<"1.6.0"),
     ],
     targets: [
         .target(

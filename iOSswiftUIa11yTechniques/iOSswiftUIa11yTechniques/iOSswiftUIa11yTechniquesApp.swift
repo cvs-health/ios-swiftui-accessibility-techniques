de141bf3d239c109148076f4aa9ea3954ecda43f
Adam, Paul <Paul.Adam2+cvsh@CVSHealth.com>
2026-04-27 19:42:09 -0500

    Update iOSswiftUIa11yTechniquesApp.swift

--- iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniquesApp.swift
+++ iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniques/iOSswiftUIa11yTechniquesApp.swift
@@ -1,5 +1,5 @@
 /*
-   Copyright 2023 CVS Health and/or one of its affiliates
+   Copyright 2023-2026 CVS Health and/or one of its affiliates
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ struct iOSswiftUIa11yTechniquesApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.locale, Locale(identifier: "es"))
         }
     }
     
